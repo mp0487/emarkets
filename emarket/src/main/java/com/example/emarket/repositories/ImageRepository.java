e460d55ecd51ed472b2f3982331d9da25ec8949a
mp0487 <115541125+mp0487@users.noreply.github.com>
2022-12-09 18:22:54 +0400

    Revert "emarkets"
    This reverts commit fd4e51f8b44186888d4209b67d92a2a8a64d7319.

--- emarket/src/main/java/com/example/emarket/repositories/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.emarket.repositories;
-
-import com.example.emarket.models.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
